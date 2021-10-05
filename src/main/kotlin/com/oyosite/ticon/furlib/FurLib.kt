0dbbaeb971df2dd60d6add1a61b8f9ecef4ab053
Ned Hunter <eehuntercats@gmail.com>
2021-10-05 15:31:54 -0400

    initial commit

---
+++ src/main/kotlin/com/oyosite/ticon/furlib/FurLib.kt
@@ -0,0 +1,8 @@
+package com.oyosite.ticon.furlib
+import net.fabricmc.api.ModInitializer
+object FurLib: ModInitializer {
+    private const val MOD_ID = "furlib"
+    override fun onInitialize() {
+
+    }
+}
