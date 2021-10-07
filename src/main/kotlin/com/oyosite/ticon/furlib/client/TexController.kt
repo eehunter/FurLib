8c396cd554c5fefd8b35c2384bb76fb8ac12ce51
Ned Hunter <eehuntercats@gmail.com>
2021-10-07 15:29:25 -0400

    First working prototype

--- src/main/kotlin/com/oyosite/ticon/furlib/client/TexController.kt
+++ src/main/kotlin/com/oyosite/ticon/furlib/client/TexController.kt
@@ -4,7 +4,7 @@ import net.minecraft.util.Identifier
 import io.github.apace100.calio.data.SerializableData as SD
 import io.github.apace100.calio.data.SerializableData.Instance as SDI
 
-class TexController(val tex:Identifier, val r:Int, val g:Int, val b:Int, val a:Int, val illuminate:Boolean = false){
+class TexController(val tex:Identifier, var r:Int, var g:Int, val b:Int, val a:Int, val illuminate:Boolean = false){
     constructor(data:SDI) : this(data.getId("tex"),data.getInt("r"),data.getInt("g"),data.getInt("b"),data.getInt("a"),data.getBoolean("illuminate"))
     companion object {fun toData(format:SD,tc:TexController):SDI{ val sdi:SDI = format.Instance();sdi["tex"]=tc.tex;sdi["r"]=tc.r;sdi["g"]=tc.g;sdi["b"]=tc.b;sdi["a"]=tc.a;sdi["illuminate"]=tc.illuminate;return sdi }}
 }
