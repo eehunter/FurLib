742af14981a5714682ed709ce88965917506c5b2
Ned Hunter <eehuntercats@gmail.com>
2021-10-06 21:29:53 -0400

    Even more setup stuff

--- src/main/kotlin/com/oyosite/ticon/furlib/client/TexController.kt
+++ src/main/kotlin/com/oyosite/ticon/furlib/client/TexController.kt
@@ -4,7 +4,7 @@ import net.minecraft.util.Identifier
 import io.github.apace100.calio.data.SerializableData as SD
 import io.github.apace100.calio.data.SerializableData.Instance as SDI
 
-class TexController(val tex:Identifier, val r:Float, val g:Float, val b:Float, val a:Float, val illuminate:Boolean = false){
-    constructor(data:SDI) : this(data.getId("tex"),data.getFloat("r"),data.getFloat("g"),data.getFloat("b"),data.getFloat("a"))
-    companion object {fun toData(format:SD,tc:TexController):SDI{ val sdi:SDI = format.Instance();sdi["tex"]=tc.tex;sdi["r"]=tc.r;sdi["g"]=tc.g;sdi["b"]=tc.b;sdi["a"]=tc.a;return sdi }}
+class TexController(val tex:Identifier, val r:Int, val g:Int, val b:Int, val a:Int, val illuminate:Boolean = false){
+    constructor(data:SDI) : this(data.getId("tex"),data.getInt("r"),data.getInt("g"),data.getInt("b"),data.getInt("a"),data.getBoolean("illuminate"))
+    companion object {fun toData(format:SD,tc:TexController):SDI{ val sdi:SDI = format.Instance();sdi["tex"]=tc.tex;sdi["r"]=tc.r;sdi["g"]=tc.g;sdi["b"]=tc.b;sdi["a"]=tc.a;sdi["illuminate"]=tc.illuminate;return sdi }}
 }
--- src/main/kotlin/com/oyosite/ticon/furlib/data/FLSerializableDataTypes.kt
+++ src/main/kotlin/com/oyosite/ticon/furlib/data/FLSerializableDataTypes.kt
@@ -7,7 +7,7 @@ import io.github.apace100.calio.data.SerializableDataType as SDT
 import io.github.apace100.calio.data.SerializableDataTypes as SDTs
 
 object FLSerializableDataTypes {
-    val TEX_CONTROLLER:SDT<TexController> = SDT.compound(TexController::class.java, SDKotlin()("tex", SDTs.IDENTIFIER)("r",SDTs.FLOAT,1f)("g",SDTs.FLOAT,1f)("b",SDTs.FLOAT,1f)("a",SDTs.FLOAT,1f),::TexController,TexController::toData)
+    val TEX_CONTROLLER:SDT<TexController> = SDT.compound(TexController::class.java, SDKotlin()("tex", SDTs.IDENTIFIER)("r",SDTs.INT,255)("g",SDTs.INT,255)("b",SDTs.INT,255)("a",SDTs.INT,255)("illuminate",SDTs.BOOLEAN,false),::TexController,TexController::toData)
     val TEX_CONTROLLERS:SDT<List<TexController>> = SDT.list(TEX_CONTROLLER)
     //operator fun get(s:String) {println(s)}
 }
